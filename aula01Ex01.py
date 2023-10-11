dbd7a76964c43d95a2bcb7e8ec971b0c540185b9
ElizaMSOliveira <elizamsoliveira@gmail.com>
2023-10-11 11:23:34 -0300

    Exercicio 1

---
+++ aula01Ex01.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome')
+salario = float(input('Digite o seu Salário:'))
+aulas = int(input('Quanta aula ministrou no mes? '))
+print('Nome: {} \nSalário: R$ {} \nAulas Ministradas: {}'.format(nome, salario, aulas))
